774d47d43c6ea101967cba811197eef3df5bd645
uniqueuser-repo <57453461+uniqueuser-repo@users.noreply.github.com>
2019-11-07 16:52:41 -0500

    scraper.py support increase
    Increased support in scraper.py. Returns None if the dining court is closed that day.

---
+++ datasetCreator.py
@@ -0,0 +1,9 @@
+from scraper import scrapedList
+
+# do some kind of matching so that we only send scrapedList valid urls from DC site
+
+# turn orderedDict into list with only value mappings
+
+# append value mapped list into 2d dataset array
+
+scrapedList("https://dining.purdue.edu/menus/Wiley/2019/11/17/Dinner")
--- scraper.py
+++ scraper.py
@@ -3,22 +3,25 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from collections import OrderedDict
 
-def scrapedList(url_input):  #returns a list of matched foods
+def scrapedList(url_input):  # returns a value listing of matched foods or "NONE" if dining court is closed
     goods = ["orange chicken", "popcorn chicken", "breaded chicken tenders", "crispy pepperoni pizza", "hamburgers", "popcorn shrimp", "crispy meat lovers pizza", "sloppy joe",
              "made to order piedmont pasta bar", "sloppy joe", "mini corn dogs", "steak house coulotte beef wiley", "clam strips", "spicy fried cheese ravioli", "meat lover's pizza",
              "tempura sweet & sour chicken", "macaroni shells and cheese", "fraldinha beef", "cream cheese wonton", "crispy jalapeno popper pizza", "crispy mac n cheese pizza",
              "fajita seasoned beef with vegetables", "general tso chicken", "hamburger", "mozzarella sticks"]
 
-    returnList = {}
+    returnOrderedDict = OrderedDict() # key-value mapping OrderedDict
 
     for i in range(len(goods)): # map each food to an index, mark 0 for nonexistent
-        returnList[goods[i]] = 0
+        returnOrderedDict[goods[i]] = 0
 
 
     url = url_input
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get(url)
     html = driver.page_source
+
+    if html.count("Bummer!"):
+        return None              # returns "none" if the DC is closed that day
 
 
     soup = BeautifulSoup(html, 'html.parser')
@@ -27,10 +30,10 @@ def scrapedList(url_input):  #returns a list of matched foods
         elementString = str(element.contents[0].encode('utf-8'))
         elementString = elementString[2:-1]
         if goods.count(elementString.lower()) != 0:
-            returnList[elementString] = 1         # if the food exists at the dining court on that day, change the value of the food's mapping to 1.
+            returnOrderedDict[elementString] = 1         # if the food exists at the dining court on that day, change the value of the food's mapping to 1.
 
-    return returnList
+    returnedList = []
+    for i in range(len(returnOrderedDict)):
+        returnedList.append(list(returnOrderedDict.items())[i][1])
 
-returnedDict = scrapedList("https://dining.purdue.edu/menus/Wiley/2019/11/7/Dinner/")
-
-print("Done!")
+    return returnedList
