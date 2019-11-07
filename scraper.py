416dd5a77a210f278aa1047c63ce020da25e0bf0
uniqueuser-repo <57453461+uniqueuser-repo@users.noreply.github.com>
2019-11-07 15:28:24 -0500

    Add specifying for foods I like

--- scraper.py
+++ scraper.py
@@ -16,6 +16,12 @@ soup = BeautifulSoup(html, 'html.parser')
 #print("len soup = " + str(len(soup)))
 #print("soup = " + str(soup))
 
+goods = ["orange chicken", "popcorn chicken", "breaded chicken tenders", "crispy pepperoni pizza", "hamburgers", "popcorn shrimp", "crispy meat lovers pizza", "sloppy joe",
+         "made to order piedmont pasta bar", "sloppy joe", "mini corn dogs", "steak house coulotte beef wiley", "clam strips", "spicy fried cheese ravioli", "meat lover's pizza",
+         "tempura sweet & sour chicken", "macaroni shells and cheese", "fraldinha beef", "cream cheese wonton", "crispy jalapeno popper pizza", "crispy mac n cheese pizza",
+         "fajita seasoned beef with vegetables", "general tso chicken", "hamburger", "mozzarella sticks"]
+
 for element in soup.find_all('span', {'class':'station-item-text'}):
     elementString = element.contents[0]
-    print(elementString)
+    if goods.count(elementString) != 0:
+        print(elementString)
