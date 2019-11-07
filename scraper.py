d72603db5314d1bbe5c48355bf61d18e1ebbe0bc
uniqueuser-repo <57453461+uniqueuser-repo@users.noreply.github.com>
2019-11-07 15:08:16 -0500

    Creating scraper for food
    Created scraper for food. html correctly retrieved, but parser currently not working.

---
+++ scraper.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+url = "https://dining.purdue.edu/menus/Wiley/2019/11/7/"
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get(url)
+html = driver.page_source
+
+#print("len html = " + str(len(html)))
+#print("html = " + html)
+
+soup = BeautifulSoup(html, 'html.parser')
+
+print("len soup = " + str(len(soup)))
+print("soup = " + str(soup))
