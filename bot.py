a5baedf95c88865bd8eaedfe33c1d31044394c0b
uniqueuser-repo <57453461+uniqueuser-repo@users.noreply.github.com>
2019-11-06 23:40:21 -0500

    Fleshing out bot.py

---
+++ bot.py
@@ -0,0 +1,27 @@
+import os
+import discord
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD= os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        print(
+            f'{client.user} is connected to the following guild:\n'
+            f'{guild.name}(id: {guild.id})'
+        )
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send("Hi, ") + member.nick + ", welcome to my Discord server!"
+
+client.run(TOKEN)
