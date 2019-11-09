b979adec093c7d76e84ac19fc5a5f640d1d71ee9
uniqueuser-repo <57453461+uniqueuser-repo@users.noreply.github.com>
2019-11-09 16:18:15 -0500

    Reorder code in datasetCreator.py

--- datasetCreator.py
+++ datasetCreator.py
@@ -1,19 +1,21 @@
 from scraper import scrapedList
 import pandas as pd
 
+# do some kind of matching so that we only send scrapedList valid urls from DC site
+
 # calendar valid days: 09/16 - 09/30
 #                      10/01 - 10/31
 #                      11/01 - 11/17
 
 # ordered dict setup: https://i.imgur.com/KI8i9b5.png
 
-dataset = []
-
 def scrapeDiningCourt(diningCourt, day, month):
-    returnVal_ScrapedList = scrapedList("https://dining.purdue.edu/menus/" + diningCourt + "/2019/" + str(month).zfill(2) + "/" + str(i).zfill(2) + "/Dinner")
+    returnVal_ScrapedList = scrapedList("https://dining.purdue.edu/menus/" + diningCourt + "/2019/" + str(month).zfill(2) + "/" + str(day).zfill(2) + "/Dinner")
     if not (returnVal_ScrapedList is None):
         dataset.append(returnVal_ScrapedList)
 
+
+dataset = []
 
 for day in range(16, 31, 1):               # covers Wiley, Hillenbrand, Ford, Windsor DC from 09/16 - 09/30
     scrapeDiningCourt("Wiley", day, 9)
